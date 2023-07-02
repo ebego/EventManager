b3d89e48a72886cb5c3f24dc0432a96082b7d21b
Erdit <erditbego@gmail.com>
2023-07-02 14:20:06 +0200

    initial seed data

--- src/main/java/com/eventmanager/api/configuration/InitialDataRunner.java
+++ src/main/java/com/eventmanager/api/configuration/InitialDataRunner.java
@@ -41,8 +41,17 @@ public class InitialDataRunner implements CommandLineRunner {
 
         createIfNotPresent("Enrique Iglesias", "The first event of year!", "2023-09-26", adminUser.getId(), 10000, "eiglesias.jpg");
         createIfNotPresent("Ricky Martin", "The second event of year!", "2023-10-26", adminUser.getId(), 100000, "eiglesias.jpg");
-        createIfNotPresent("Sinan Hoxha", "The third event of year!", "2023-12-12", adminUser.getId(), 1000, "eiglesias.jpg");
-        createIfNotPresent("Elvana Gjata", "The fourth event of year!", "2024-01-01", adminUser.getId(), 10000, "eiglesias.jpg");
+        createIfNotPresent("Sinan Hoxha", "The third event of year!", "2023-12-12", adminUser.getId(), 1000, "shoxha.jpeg");
+        createIfNotPresent("Elvana Gjata", "The fourth event of year!", "2024-01-01", adminUser.getId(), 10000, "egjata.jpg");
+        createIfNotPresent("Event11", "The eleventh event of year!", "2023-07-15", adminUser.getId(), 10000, "event11.jpeg");
+        createIfNotPresent("Event5", "The fifth event of year!", "2023-09-10", adminUser.getId(), 10000, "event5.jpeg");
+        createIfNotPresent("Event6", "The sixth event of year!", "2024-08-25", adminUser.getId(), 10000, "event6.jpeg");
+        createIfNotPresent("Event7", "The seventh event of year!", "2024-03-12", adminUser.getId(), 10000, "event7.jpeg");
+        createIfNotPresent("Event8", "The eighth event of year!", "2024-12-08", adminUser.getId(), 10000, "event8.jpeg");
+        createIfNotPresent("Event9", "The ninth event of year!", "2024-06-24", adminUser.getId(), 10000, "event9.jpeg");
+        createIfNotPresent("Event10", "The tenth event of year!", "2024-04-30", adminUser.getId(), 10000, "event10.jpeg");
+        createIfNotPresent("Event12", "The twelfth event of year!", "2023-04-30", adminUser.getId(), 10000, "event12.jpeg");
+        createIfNotPresent("Event13", "The thirteenth event of year!", "2023-05-20", adminUser.getId(), 10000, "event13.jpeg");
     }
 
     public void createIfNotPresent(String title, String description, String date, int ownerId, int maxBooking, String banner) {
